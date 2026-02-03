12106726ebe7a87c1e52fe03374ac30dc80b18d7
John Lombardo <hello@johnlombardo.dev>
2026-02-03 23:42:38 +0800

    fix(new): build entry

--- packages/new/bunup.config.ts
+++ packages/new/bunup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'bunup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['index.ts'],
   outDir: 'dist',
   format: ['esm', 'cjs'],
   target: 'node',
