95a76809304c0a13741695b9611b2b4b26c916cf
John Lombardo <hello@johnlombardo.dev>
2026-02-03 17:42:48 +0800

    chore: remove legacy template files

--- apps/cli-tool/src/index.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bun
-
-console.log('CLI Tool is running!')
-console.log('This is a basic CLI tool template.')
--- packages/config/prettier/index-tailwind.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('prettier').Config} */
-const baseConfig = require('./index.js')
-
-module.exports = {
-  ...baseConfig,
-  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['cx', 'tv', 'cn', 'cnJoin', 'twMerge', 'twJoin'],
-}
